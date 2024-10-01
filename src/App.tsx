d04c1bdfc428636aaae0c716a8bae332d438090e
Paul L. <13600558+dev-paulL@users.noreply.github.com>
2024-10-01 11:06:21 +0200

    add: mobile form, structure & style

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,30 @@ function App() {
         <source srcSet={headerPictureDesktop} media="(min-width: 60rem)" />
         <img src={headerPictureMobile} alt="" />
       </picture>
-
-      <h1>Stay updated!</h1>
-      <p>Join 60,000+ product managers receiving monthly updates on:</p>
-      <ul>
-        <li>Product discovery and building what matters</li>
-        <li>Measuring to ensure updates are a success</li>
-        <li>And much more!</li>
-      </ul>
-      <form>
-        <label htmlFor="email">Email address</label>
-        <input type="email" placeholder="email@company.com" id="email" />
-        <button type="submit">Subscribe to monthly newsletter</button>
+      <form className="p-6 flex flex-col gap-4">
+        <h1 className="text-3xl font-bold text-grey800">Stay updated!</h1>
+        <p>Join 60,000+ product managers receiving monthly updates on:</p>
+        <ul className="flex flex-col gap-2 listgroup">
+          <li>Product discovery and building what matters</li>
+          <li>Measuring to ensure updates are a success</li>
+          <li>And much more!</li>
+        </ul>
+        <div className="flex flex-col gap-1 mt-2">
+          <label htmlFor="email" className="text-xs font-bold">Email address</label>
+          <input
+            type="email"
+            placeholder="email@company.com"
+            id="email"
+            required
+            className="p-3 border border-grey200 rounded-lg"
+          />
+        </div>
+        <button
+          type="submit"
+          className="cursor-pointer bg-grey600 text-white font-bold p-3 rounded-md"
+        >
+          Subscribe to monthly newsletter
+        </button>
       </form>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tomato: "hsl(4, 100%, 67%)",
+        grey800: "hsl(234, 29%, 20%)",
+        grey600: " hsl(235, 18%, 26%)",
+        grey200: "hsl(231, 7%, 60%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        roboto: "Roboto",
+      },
+    },
   },
   plugins: [],
 };
